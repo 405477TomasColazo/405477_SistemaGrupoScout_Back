13c43ba44b6b92c816f1f3fadc4745d75546e71a
405477TomasColazo <405477@tecnicatura.frc.utn.edu.ar>
2025-04-08 15:56:21 -0300

    template

---
+++ Sistema-Scouts-Jose-Hernandez/src/main/java/ar/edu/utn/frc/sistemascoutsjosehernandez/SistemaScoutsJoseHernandezApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.sistemascoutsjosehernandez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaScoutsJoseHernandezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaScoutsJoseHernandezApplication.class, args);
+    }
+
+}
